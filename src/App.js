793b3ff4e54e27d9f7c44324febac4d9cfe81860
Akhtool <Zelimhan1997@mail.ru>
2023-12-14 18:40:01 +0300

    feat: сверстать секцию со слайдером

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import headerLogo from "./images/header-logo.png";
 import headerCart from "./images/header-cart.svg";
 import headerFavorite from "./images/header-favorite.svg";
 import headerProfile from "./images/header-profile.svg";
+import sliderLogo from "./images/slider-logo-img.png";
 
 function App() {
   return (
@@ -43,7 +44,21 @@ function App() {
           </ul>
         </div>
       </header>
-      <main></main>
+      <main>
+        <section className="slider">
+          <div className="slider__container">
+            <div className="slider__text-block">
+              <div className="slider__link">
+                <img alt="logotype" width={100} height={40} src={sliderLogo} />
+              </div>
+              <h2 className="slider__title">
+                <span className="slider__title-span">Stan Smith</span>Forever!
+              </h2>
+              <button className="slider__button">Купить</button>
+            </div>
+          </div>
+        </section>
+      </main>
       <footer></footer>
     </div>
   );
