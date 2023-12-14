869249f62e492edf331f2de4a4501a0382032772
Akhtool <Zelimhan1997@mail.ru>
2023-12-14 16:08:18 +0300

    feat: сверстать шапку

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import headerLogo from "./images/header-logo.png";
+import headerCart from "./images/header-cart.svg";
+import headerFavorite from "./images/header-favorite.svg";
+import headerProfile from "./images/header-profile.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <div className="header__left">
+          <img className="header__logo" alt="logoype" src={headerLogo} />
+          <div className="header__container">
+            <h3 className="heder__title">REACT SNEAKERS</h3>
+            <p className="header__subtitle">Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <div className="header__right">
+          <ul className="header__nav-container">
+            <li className="header__cart header__nav-item">
+              <img
+                className="header__nav-item-img"
+                alt="cart-log"
+                src={headerCart}
+              />
+              <span>1205р.</span>
+            </li>
+            <li className="header__favourites header__nav-item">
+              <img
+                className="header__nav-item-img"
+                alt="cart-log"
+                src={headerFavorite}
+              />
+              <span>Избранное</span>
+            </li>
+            <li className="header__profile header__nav-item">
+              <img
+                className="header__nav-item-img"
+                alt="cart-log"
+                src={headerProfile}
+              />
+              <span>Профиль</span>
+            </li>
+          </ul>
+        </div>
       </header>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
