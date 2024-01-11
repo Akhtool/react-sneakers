af4ee77268fb42b084acbc1ea48022ce1e3e0740
Akhtool <Zelimhan1997@mail.ru>
2024-01-11 20:07:57 +0300

    feat: реализовать поиск по карточкам

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,13 +1,23 @@
 import "./Cards.css";
 import Card from "../Card/Card";
+import closeBtn from "../../images/close.svg";
 
 import searchLogo from "../../images/search.svg";
+import { useState } from "react";
 
 function Cards({ isLoading, cards }) {
+  const [searchValue, setSearchValue] = useState("");
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <section className="cards">
       <div className="cards__top">
-        <h1 className="cards__top-title">Все кроссовки</h1>
+        <h1 className="cards__top-title">
+          {searchValue ? `Поиск по запросу: "${searchValue}"` : "Все кроссовки"}
+        </h1>
         <div className="cards__top-search">
           <img
             className="cards__top-search-logo"
@@ -16,22 +26,41 @@ function Cards({ isLoading, cards }) {
             width={20}
             height={20}
           />
-          <input className="cards__input" placeholder="Поиск..." />
+          {searchValue && (
+            <img
+              className="cards__top-search-btn"
+              src={closeBtn}
+              alt="Remove"
+              onClick={() => setSearchValue("")}
+            />
+          )}
+          <input
+            className="cards__input"
+            placeholder="Поиск..."
+            value={searchValue}
+            onChange={onChangeSearchValue}
+          />
         </div>
       </div>
       <div className="cards__list">
-        {cards.map((item) => {
-          return (
-            <Card
-              key={item.id}
-              isLoading={isLoading}
-              id={item.id}
-              imageUrl={item.imageUrl}
-              title={item.title}
-              price={item.price}
-            />
-          );
-        })}
+        {cards
+          .filter((item) =>
+            item.title
+              .toLocaleLowerCase()
+              .includes(searchValue.toLocaleLowerCase())
+          )
+          .map((item) => {
+            return (
+              <Card
+                key={item.id}
+                isLoading={isLoading}
+                id={item.id}
+                imageUrl={item.imageUrl}
+                title={item.title}
+                price={item.price}
+              />
+            );
+          })}
       </div>
     </section>
   );
